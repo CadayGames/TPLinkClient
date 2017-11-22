efed853a72f51399ded162853dfd93bc2ffe6e1e
CadayGames <CadayGames@gmail.com>
2017-11-22 21:10:38 +0100

    First release

---
+++ TPLinkClient/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace TPLinkClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelWanIP = new System.Windows.Forms.Label();
+            this.labelUptime = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBarLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.labelStatus, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelWanIP, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelUptime, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(288, 129);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelStatus.Location = new System.Drawing.Point(118, 0);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(76, 43);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "unknown";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(44, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(34, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 43);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "WAN IP:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(39, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uptime:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelWanIP
+            // 
+            this.labelWanIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelWanIP.AutoSize = true;
+            this.labelWanIP.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelWanIP.Location = new System.Drawing.Point(118, 43);
+            this.labelWanIP.Name = "labelWanIP";
+            this.labelWanIP.Size = new System.Drawing.Size(76, 43);
+            this.labelWanIP.TabIndex = 4;
+            this.labelWanIP.Text = "unknown";
+            this.labelWanIP.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelUptime
+            // 
+            this.labelUptime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelUptime.AutoSize = true;
+            this.labelUptime.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelUptime.Location = new System.Drawing.Point(118, 86);
+            this.labelUptime.Name = "labelUptime";
+            this.labelUptime.Size = new System.Drawing.Size(76, 43);
+            this.labelUptime.TabIndex = 5;
+            this.labelUptime.Text = "unknown";
+            this.labelUptime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 169);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(312, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusBarLabel
+            // 
+            this.statusBarLabel.Name = "statusBarLabel";
+            this.statusBarLabel.Size = new System.Drawing.Size(150, 17);
+            this.statusBarLabel.Text = "Nawiązywanie połączenia...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 191);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP-Link Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.MainForm_PreviewKeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelWanIP;
+        private System.Windows.Forms.Label labelUptime;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarLabel;
+    }
+}
+
---
+++ TPLinkClient/MainForm.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace TPLinkClient
+{
+    public partial class MainForm : Form
+    {
+        public static MainForm mainWindow;
+        public TPLinkTelnet telnet;
+        public Thread updateThread;
+        public LabelUptimeTimer labelUptimeTimer;
+
+        public class RouterInfo
+        {
+            public static string IP = "192.168.1.1";
+            public static int Port = 23;
+            public static string Username = "admin";
+            public static string Password = "admin";
+            public static string WANInterface = "pppoe_0_35_3_d";
+        }
+
+        public MainForm()
+        {
+            mainWindow = this;
+
+            InitializeComponent();
+            
+            labelUptimeTimer = new LabelUptimeTimer();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            UpdateRouterInfo();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            updateThread.Abort();
+            labelUptimeTimer.Stop();
+        }
+
+        public void UpdateRouterInfo()
+        {
+            if (updateThread != null)
+                updateThread.Abort();
+
+            updateThread = new Thread(UpdateRouterInfoEntry);
+            updateThread.Start();
+        }
+
+        private bool iniReaded = false;
+
+        public void ReadIniFile(string filename)
+        {
+            if (iniReaded)
+                return;
+            
+            if (System.IO.File.Exists(filename))
+            {
+                string[] lines = System.IO.File.ReadAllLines(filename);
+                foreach (string line in lines)
+                {
+                    string[] varValue = line.Split(new char[]{'='}, 2);
+                    if (varValue.Length == 2)
+                    {
+                        string var = varValue[0].ToLower();
+                        string value = varValue[1];
+                        switch (var)
+                        {
+                            case "ip": RouterInfo.IP = value; break;
+                            case "port":
+                                if (int.TryParse(value, out int valueInt))
+                                    RouterInfo.Port = valueInt;
+                                break;
+                            case "username": RouterInfo.Username = value; break;
+                            case "password": RouterInfo.Password = value; break;
+                            case "waninterface": RouterInfo.WANInterface = value; break;
+                        }
+                    }
+                }
+            }
+
+            iniReaded = true;
+        }
+
+        public void UpdateRouterInfoEntry()
+        {
+            ReadIniFile(Application.ProductName + ".ini");
+
+            try
+            {
+                telnet = new TPLinkTelnet(RouterInfo.IP, RouterInfo.Port);
+                telnet.UpdateInfo(RouterInfo.Username, RouterInfo.Password);
+                telnet.client.Close();
+            }
+            catch (SocketException)
+            {
+                SetStatusBarLabel("Brak połączenia sieciowego.");
+            }
+        }
+
+        public static void SetStatusBarLabel(string text)
+        {
+            mainWindow.Invoke((MethodInvoker) delegate 
+            {
+                mainWindow.statusBarLabel.Text = text;
+            });
+        }
+
+        public class TPLinkTelnet
+        {
+            public static int WAIT_DELAY_MS = 20;
+            public static int RESPONSE_SIZE = 4096;
+
+            public TcpClient client = null;
+            public NetworkStream ns = null;
+
+            public TPLinkTelnet(string ip, int port)
+            {
+                client = new TcpClient(ip, port);
+                ns = client.GetStream();
+            }
+            
+            byte[] responseBytes = new byte[RESPONSE_SIZE];
+            string response = string.Empty;
+            string responseBytesStr = string.Empty;
+            int bytesRead = 0;
+
+            private int ReadResponseBytes(int max_loops = 10)
+            {
+                for (int i = 0; i < max_loops; i++)
+                {
+                    if (ns.DataAvailable)
+                    {
+                        bytesRead = ns.Read(responseBytes, 0, RESPONSE_SIZE);
+
+                        if (bytesRead > 0)
+                        {
+                            responseBytesStr = Encoding.UTF8.GetString(responseBytes, 0, bytesRead);
+                            //Console.WriteLine("bytesRead:{0};responseBytesStr:{1};", bytesRead, responseBytesStr);
+                            return bytesRead;
+                        }
+                    }
+
+                    //Console.WriteLine("Waiting for response...");
+                    Thread.Sleep(WAIT_DELAY_MS);
+                }
+
+                return 0;
+            }
+
+            public void ReadMessage()
+            {
+                if (ReadResponseBytes() > 0)
+                    response += responseBytesStr;
+            }
+
+            public bool ReadMessageRegex(string regex, int max_loops = 5)
+            {
+                ClearMessage();
+
+                for (int i = 0; i < max_loops; i++)
+                {
+                    ReadMessage();
+
+                    if (CheckMessageRegex(regex))
+                        return true;
+                }
+
+                return false;
+            }
+
+            public void ClearMessage()
+            {
+                response = string.Empty;
+                responseBytesStr = string.Empty;
+            }
+
+            public void SendMessage(string message)
+            {
+                byte[] commandBytes = Encoding.UTF8.GetBytes(message + "\r\n");
+                ns.Write(commandBytes, 0, commandBytes.Length);
+            }
+
+            public string GetMessageInfo(string var)
+            {
+                Regex regex = new Regex("^" + var + "=(.+)\r\n", RegexOptions.Multiline);
+                Match match = regex.Match(response);
+                if (match.Groups.Count >= 2)
+                    return match.Groups[1].ToString();
+                else
+                    return string.Empty;
+            }
+
+            public bool CheckMessageRegex(string regex)
+            {
+                if (new Regex(regex).IsMatch(response))
+                    return true;
+                else
+                    return false;
+            }
+
+            public void UpdateInfo(string username, string password)
+            {
+                SetStatusBarLabel("Nawiązywanie połączenia...");
+
+                if (!ReadMessageRegex("username:"))
+                {
+                    Console.WriteLine("Nie znaleziono regex w response. (username)");
+
+                    if (CheckMessageRegex("Authorization failed"))
+                    {
+                        SetStatusBarLabel("Przekroczono limit nieprawidłowych prób logowania.");
+                        return;
+                    }
+
+                    SetStatusBarLabel("Nie udało się ustanowić połączenia z routerem.");
+                    return;
+                }
+                
+                // type username
+                
+                SendMessage(username);
+                
+                if (!ReadMessageRegex("password:"))
+                {
+                    Console.WriteLine("Nie znaleziono regex w response. (password)");
+                    SetStatusBarLabel("Coś poszło nie tak przy logowaniu.");
+                    return;
+                }
+
+                // type password
+                
+                SendMessage(password);
+
+                if (!ReadMessageRegex("\\#"))
+                {
+                    Console.WriteLine("Nie znaleziono regex w response. (# -> password)");
+
+                    if (CheckMessageRegex("Login incorrect"))
+                    {
+                        SetStatusBarLabel("Dane uwierzytelniające są nieprawidłowe.");
+                        return;
+                    }
+
+                    SetStatusBarLabel("Nie udało się zalogować.");
+
+                    return;
+                }
+
+                // wan show connection info
+                
+                SendMessage("wan show connection info " + RouterInfo.WANInterface);
+
+                if (!ReadMessageRegex("\\#"))
+                {
+                    Console.WriteLine("Nie znaleziono regex w response. (# -> wan show connection info)");
+                    SetStatusBarLabel("Nie udało się pobrać informacji o interfejsie WAN.");
+                    return;
+                }
+
+                // wan info
+
+                String uptime = GetMessageInfo("uptime");
+                String connectionStatus = GetMessageInfo("connectionStatus");
+                String externalIPAddress = GetMessageInfo("externalIPAddress");
+                
+                mainWindow.Invoke((MethodInvoker) delegate {
+                    mainWindow.labelStatus.Text = connectionStatus;
+                    mainWindow.labelWanIP.Text = externalIPAddress;
+                });
+
+                int.TryParse(uptime, out int uptimeSeconds);
+
+                mainWindow.labelUptimeTimer.Update(uptimeSeconds);
+
+                SetStatusBarLabel("Zakończono pobieranie danych.");
+
+                // logout
+
+                SendMessage("logout");
+                
+                if (!ReadMessageRegex("Bye"))
+                {
+                    Console.WriteLine("Nie znaleziono regex w response. (Bye -> logout)");
+                }
+            }
+        }
+
+        public class LabelUptimeTimer
+        {
+            public int seconds = 0;
+            public System.Timers.Timer timer;
+
+            public LabelUptimeTimer()
+            {
+                timer = new System.Timers.Timer
+                {
+                    Interval = 1000,
+                    AutoReset = true
+                };
+
+                timer.Elapsed += Timer_Elapsed;
+            }
+
+            public void Start()
+            {
+                timer.Start();
+            }
+
+            public void Stop()
+            {
+                timer.Stop();
+            }
+
+            public void Update(int seconds)
+            {
+                if (!timer.Enabled)
+                    timer.Start();
+
+                this.seconds = seconds;
+                UpdateLabel();
+            }
+
+            private void UpdateLabel()
+            {
+                String uptime = string.Format("{0:00}:{1:00}:{2:00}",
+                    seconds / 3600, (seconds / 60) % 60, seconds % 60);
+
+                mainWindow.Invoke((MethodInvoker)delegate {
+                    mainWindow.labelUptime.Text = uptime;
+                });
+            }
+
+            private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                seconds++;
+                UpdateLabel();
+            }
+        }
+        
+        private void MainForm_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                //Console.WriteLine("Updating...");
+                UpdateRouterInfo();
+            }
+        }
+    }
+}
